c9a6cca1a804061710521645ff5fd777ee04d2f1
Nagalakshmi <nagalakshmi.pulivarthi@gmail.com>
2020-02-18 22:07:07 -0500

    Change in Timer

--- clock.py
+++ clock.py
@@ -1,11 +1,16 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 from emailapp import ProcessEmail
+import os
+
+hours=os.environ['Schedule'].split(":")[0]
+mins=os.environ['Schedule'].split(":")[1]
 
 sched = BlockingScheduler()
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=8,minute=45)
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=hours,minute=mins)
 def scheduled_job():
     print('Starting the job:')
     ProcessEmail()
     print('Job executed successfullys.')
+
 
 sched.start()
